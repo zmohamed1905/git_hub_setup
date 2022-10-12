cd316e08509ce940a2c7f81b96c0c22cd40c7da2
zmohamed1905 <zakariya.m1905@gmail.com>
2022-10-12 12:18:19 +0100

    made changes by adding super hero story using dict

---
+++ dict_task.py
@@ -0,0 +1,15 @@
+story_1 = {
+    "start": "Once upon a time there was a knight in shinning armour",
+    "middle": "He found someone with gun powder under the castle",
+    "end": "He found and captured the man and put him in the castle dungeon forever"
+}
+
+print(story_1)
+print(type(story_1))
+print(story_1.keys())
+print(story_1.values())
+print(story_1["start"])
+print(story_1["middle"])
+print(story_1["end"])
+story_1["hero"] = "Knight"
+print(story_1)
